59fb447da0069f052fec4fa9a1ecb481390a7a66
mannemswapna <22b01a1294@svecw.edu.in>
2024-03-13 09:23:04 +0530

    first

--- spell.py
+++ spell.py
@@ -16,6 +16,9 @@ class spell:
         self.name_lbl.place(x=50,y=100)
         self.submit_btn=Button(root,text='submit',font=('Roboto', 15),justify='center',command=self.register)
         self.submit_btn.place(x=250,y=300)
+        self.back_btn = Button(root, text='Back', font=('Roboto', 15), command=self.goBack)
+        self.back_btn.place(x=100, y=300)
+        self.corr_word = None
        
     def register(self):
         entered_word = self.name_entry.get()
@@ -27,6 +30,17 @@ class spell:
         corrected_word = spell_checker.correction(entered_word)
         self.corr_word = Label(self.root, text='Correct spelling: ' + corrected_word, font=('Roboto', 15, 'bold'))
         self.corr_word.place(x=50, y=150)
+    def goBack(self):
+        if self.corr_word:
+            self.corr_word.destroy()
+        self.title_lbl = Label(root, text='SPELLING CHECKER', font=('Roboto', 15, 'bold'))
+        self.title_lbl.place(x=180, y=0)
+        self.name_entry = Entry(root, width=15, font=('Roboto', 15, 'bold'))
+        self.name_entry.place(x=300, y=100)
+        self.name_lbl = Label(root, text='Enter a reference word : ', font=('Roboto', 15, 'italic'))
+        self.name_lbl.place(x=50, y=100)
+        self.submit_btn = Button(root, text='Submit', font=('Roboto', 15), command=self.register)
+        self.submit_btn.place(x=250, y=300)
 root = Tk()
 root.geometry('600x400+400+150')
 root.title('spell')
